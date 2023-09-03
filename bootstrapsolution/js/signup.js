d05a1842d31ff5f680d68b04d8403b6e04de7585
Abed <abed.hajqasem@hotmail.com>
2023-09-03 20:49:41 +0300

    welcomepage

--- bootstrapsolution/js/signup.js
+++ bootstrapsolution/js/signup.js
@@ -2,12 +2,15 @@
 
 let btn = document.getElementById("submit");
 btn.addEventListener("click", function(event) {
+  let flag=true;
   let Elements = document.getElementsByClassName("required");
 
   Array.from(Elements).forEach(element => {
     if (element.value === "") {
       let span = document.getElementById(element.name);
+      console.log(span);
       span.innerText = "Required";
+      flag=false;
     }
     else{
       let span = document.getElementById(element.name);
@@ -19,25 +22,27 @@ btn.addEventListener("click", function(event) {
   let email=document.getElementById("mail");
   if(phone.value.length!==10){
     let span = document.getElementById(phone.name);
-
     span.innerText = "The mobile number should contain 10 digits ";
+    flag=false;
   }
   else{
     let span = document.getElementById(phone.name);
     span.innerText = "";
+  
   }
   if(!email.value.includes("@")){
     let span = document.getElementById(email.name);
     span.innerText = "email should contain '@'";
+    flag=false;
   }
   else{
     let span = document.getElementById(phone.name);
     span.innerText = "";
   }
-
   event.preventDefault();
-    const firstName = document.getElementById("firstname").value;
+if(flag){
+    const firstName = document.getElementById("firstname1").value;
     window.location.href = `/bootstrapsolution/welcome.html?firstname=${encodeURIComponent(firstName)}`;
-    
+  }
   
 });
